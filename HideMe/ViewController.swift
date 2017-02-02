9030c76b4de13fa3195aca56974085518593eb3b
Yves Binda <yves.i.binda@gmail.com>
2017-02-02 12:19:16 -0500

    added files for hideme

--- HideMe/ViewController.swift
+++ HideMe/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blue: UIImageView!
+    @IBOutlet weak var Red: UIImageView!
+    @IBOutlet weak var HideRedButton: UIButton!
+    
+    @IBOutlet weak var HideBlueButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HideBlueBack(_ sender: Any) { blue.isHidden = true
+    }
 
+    @IBAction func HideRedBack(_ sender: Any) { Red.isHidden = true
+    }
 }
 
